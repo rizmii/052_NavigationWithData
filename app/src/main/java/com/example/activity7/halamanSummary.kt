7c7dade7808b5a0c4ba4944efea4b60f9061d493
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-11-16 14:33:46 +0700

    add function halamanSummary

--- app/src/main/java/com/example/activity7/halamanSummary.kt
+++ app/src/main/java/com/example/activity7/halamanSummary.kt
@@ -1,2 +1,42 @@
 package com.example.activity7
 
+import android.widget.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.activity7.data.OrderUIState
+
+@Composable
+fun halamanSummary(
+    OrderUIState: OrderUIState,
+    onClickedButton: ()-> Unit
+){
+    Column (
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+    )
+    {
+        Text(text = "Nama")
+        Text(text = OrderUIState.nama)
+        Divider()
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(text = "Alamat")
+        Text(text = OrderUIState.alamat)
+        Divider()
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(text = "No Telepon")
+        Text(text = OrderUIState.noTelp)
+        Divider()
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        }
+    }
