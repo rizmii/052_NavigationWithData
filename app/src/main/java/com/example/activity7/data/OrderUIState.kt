06ef31b5a4a5ea041fb56d9de9f84b81f29ff435
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-11-16 14:00:18 +0700

    add data OrderUIState

--- app/src/main/java/com/example/activity7/data/OrderUIState.kt
+++ app/src/main/java/com/example/activity7/data/OrderUIState.kt
@@ -3,7 +3,10 @@ package com.example.activity7.data
 data class OrderUIState(
     val jumlah : Int =0,
     val rasa :String ="",
-    val harga : String = ""
+    val harga : String = "",
+    val nama: String = "",
+    val alamat: String="",
+    val noTelp: String=""
 )
 
 
